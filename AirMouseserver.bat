af3a293db257bcff2b41873ea2fa7fde24b33711
super cool dev <kasusaland@gmail.com>
2026-04-30 23:55:30 +0800

    hpf

--- AirMouseserver.bat
+++ AirMouseserver.bat
@@ -1,5 +1,6 @@
 @echo off
 echo 正在检查 Python 依赖...
-pip install flask flask-socketio pyOpenSSL pynput
+pip install flask flask-socketio pyOpenSSL pynput psutil 
+start https://localhost:5888
 python server.py
 pause
